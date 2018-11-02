66d4ff4751c81592c6585fe776b8c36c91fd1728
Giulia Berto <gberto@hm012.karst.uits.iu.edu>
2018-11-02 10:51:45 -0400

    initial commit

---
+++ afqConverter.m
@@ -0,0 +1,22 @@
+function out = afqConverter()
+
+if ~isdeployed
+	addpath(genpath('/N/u/brlife/git/vistasoft'));
+	addpath(genpath('/N/u/brlife/git/jsonlab'));
+	addpath(genpath('/N/u/brlife/git/o3d-code'));
+end
+
+config = loadjson('config.json');
+ref_src = fullfile(config.t1);
+
+disp('Converting wmc to .trk');
+load(fullfile(config.segmentation));
+
+for tract=1:length(fg_classified)
+    tract_name=strrep(fg_classified(tract).name,' ','_');
+    write_fg_to_trk_shift(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+    fprintf(fid, [tract_name, '\n']);
+end 
+
+exit;
+end
---
+++ resampling_segmentation.py
@@ -0,0 +1,76 @@
+from __future__ import division
+import os
+import sys
+import argparse
+import nibabel as nib
+import numpy as np
+import dipy
+from dipy.tracking.utils import length
+from dipy.tracking.streamline import set_number_of_points
+
+
+def resample_tract(tract, step_size):
+    """Resample the tract with the given step size.
+    """
+    lengths=list(length(tract))
+    tract_res = []
+    for i, f in enumerate(tract):
+	nb_res_points = np.int(np.floor(lengths[i]/step_size))
+	tmp = set_number_of_points(f, nb_res_points)
+	tract_res.append(tmp)
+    tract_res = nib.streamlines.array_sequence.ArraySequence(tract_res)
+    return tract_res
+
+
+def save_tract(tract, t1_filename, out_filename):
+	"""Save the tract in trk or tck format.
+	"""
+	extension = os.path.splitext(out_filename)[1]
+	t1 = nib.load(t1_filename)
+	aff_vox_to_ras = t1.affine
+	header = t1.header
+	dimensions = header.get_data_shape()
+	voxel_sizes = header.get_zooms()
+	
+	if extension == '.trk':
+		hdr = nib.streamlines.trk.TrkFile.create_empty_header()
+		hdr['voxel_sizes'] = voxel_sizes
+		hdr['dimensions'] = dimensions
+		hdr['voxel_order'] = 'LAS'
+		hdr['voxel_to_rasmm'] = aff_vox_to_ras 
+	elif extension == '.tck':
+		hdr = nib.streamlines.tck.TckFile.create_empty_header()
+		hdr['voxel_sizes'] = voxel_sizes
+		hdr['dimensions'] = dimensions
+	else:
+		print("%s format not supported." % extension)
+
+	t = nib.streamlines.tractogram.Tractogram(tract, affine_to_rasmm=np.eye(4))
+	nib.streamlines.save(t, out_filename, header=hdr)
+	print("Bundle saved in %s" % out_filename)
+
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-dir_trk', nargs='?', const=1, default='',
+	                    help='Directory of the trk files')   
+	parser.add_argument('-t1', nargs='?', const=1, default='',
+	                    help='T1 filename')  
+	parser.add_argument('-step_size', nargs='?', const=1, default='',
+	                    help='The step size to use for resampling (in mm)')    
+	parser.add_argument('-out_dir', nargs='?', const=1, default='',
+	                    help='The output directory')                                           
+	args = parser.parse_args()
+
+	tracts = os.listdir(args.dir_trk)
+	step_size = np.asarray(args.step_size, dtype='float64')
+
+	for i in range(len(tracts)):
+		tract = '%s/%s' %(args.dir_trk, tracts[i])
+		tract = nib.streamlines.load(tract).streamlines
+		tract_res = resample_tract(tract, step_size)
+		out_filename = '%s/%s' %(args.out_dir, tracts[i])
+		save_tract(tract_res, t1, out_filename)
+
+	sys.exit()
