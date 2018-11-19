efa4a63fdc45bd9fe5f4a674166ba27f0eee4072
Giulia Berto <gberto@hm012.karst.uits.iu.edu>
2018-11-19 07:30:30 -0500

    rename tracts

--- afqConverter.m
+++ afqConverter.m
@@ -14,7 +14,7 @@ load(fullfile(config.segmentation));
 
 for tract=1:length(fg_classified)
     tract_name=strrep(fg_classified(tract).name,' ','_');
-    write_fg_to_trk_shift(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+    write_fg_to_trk_shift(fg_classified(tract),ref_src,sprintf('%s.trk',tract_name));
 end 
 
 exit;
